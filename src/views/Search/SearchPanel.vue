a6cf0d2252ce7dd9c46e03e875bc66926c9c3713
wdx_gitee <1091830282@qq.com>
2019-11-10 17:52:41 +0800

    search结束

--- src/views/Search/SearchPanel.vue
+++ src/views/Search/SearchPanel.vue
@@ -7,6 +7,22 @@
       </div>
       <button @click="isShowSearchPanel(false)">取消</button>
     </div>
+    <div class="search_content">
+      <div class="title">
+        <img src="./images/hot.png" alt="" width="25">
+        <span>热门搜索</span>
+      </div>
+      <ul class="search_list">
+        <li>抽纸批发</li>
+        <li>浴巾可穿</li>
+        <li>石榴水果</li>
+        <li>富豪卷纸</li>
+        <li>冰箱收纳盒</li>
+        <li>毛巾衣服</li>
+        <li>小白增高透气</li>
+        <li>连衣裙</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -68,4 +84,21 @@ export default {
       font-size 16px
       color #666
       background-color #fff
+  .search_content
+    padding 30px 15px
+    .title
+      display flex
+      align-items center
+      color #aaaaaa
+      margin-bottom 10px
+    .search_list
+      display flex
+      flex-wrap wrap
+      li
+        color #aaa
+        margin 5px 10px
+        padding 5px
+        border-radius 5px
+        font-weight lighter
+        font-size 12px
 </style>
